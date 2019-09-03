13ca86a0342d543a7623633c4e324028cefffe95
Jo19r85ge <cordobescooper@gmail.com>
2019-09-03 17:03:39 +0200

    Actio-sheet noticia

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -14,7 +15,8 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
 
-  constructor( private iab: InAppBrowser) { }
+  constructor( private iab: InAppBrowser,
+               private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
   }
@@ -23,6 +25,37 @@ export class NoticiaComponent implements OnInit {
 
     console.log('Noticia', this.noticia.url);
     const browser = this.iab.create(this.noticia.url, '_system');
+
+  }
+
+ async lanzarMenu() {
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share',
+          handler: () => {
+            console.log('Share clicked');
+        }
+      },
+        {
+          text: 'Favorito',
+          icon: 'star',
+          handler: () => {
+            console.log('Favorito');
+        }
+      },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
 
   }
 
