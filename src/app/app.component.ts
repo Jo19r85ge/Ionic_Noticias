bf29e54b734340341038cc352b51d008e71fd9ff
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2019-09-30 16:12:10 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -8,16 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  constructor(
+export class AppComponent 
+{
+  constructor
+  (
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+  ) 
+    {
+      this.initializeApp();
+    }
 
-  initializeApp() {
+  initializeApp() 
+  {
     this.platform.ready().then(() => {
       this.statusBar.styleLightContent();
       this.splashScreen.hide();
