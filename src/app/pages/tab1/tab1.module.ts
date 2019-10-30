65d705e4ebb0550ad25d1a8630ab403e4c79d187
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2019-10-30 21:46:49 +0100

    Update tab1.module.ts

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -8,7 +8,8 @@ import { ComponentsModule } from '../../components/components.module';
 
 @NgModule(
   {
-  imports: [
+  imports: 
+    [
     ComponentsModule,
     IonicModule,
     CommonModule,
