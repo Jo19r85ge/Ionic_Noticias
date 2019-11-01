800df29a319b83cc02fb0086acdb0660826ee31c
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2019-11-01 20:21:42 +0100

    Update tab2.page.spec.ts

--- src/app/pages/tab2/tab2.page.spec.ts
+++ src/app/pages/tab2/tab2.page.spec.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Tab2Page } from './tab2.page';
 
+
 describe('Tab2Page', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
